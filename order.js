4a0a32b735da7480c1b344bbd9d1e702ef36da76
marion586 <marionmenye@gmail>
2022-05-29 19:06:23 +0300

    first integration

---
+++ order.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName : 'Product name 1',
+        productNumber: '85631',
+        paymentStatus : 'Due 1',
+        shipping: 'Pending'
+    },
+    {
+        productName : 'Product name 2',
+        productNumber: '85631',
+        paymentStatus : 'Due 2',
+        shipping: 'Declined'
+    },
+    {
+        productName : 'Product name 3',
+        productNumber: '85631',
+        paymentStatus : 'Due 3',
+        shipping: 'Pending'
+    },
+    {
+        productName : 'Product name 4',
+        productNumber: '85631',
+        paymentStatus : 'Due 4',
+        shipping: 'Pending'
+    },
+    {
+        productName : 'Product name 5',
+        productNumber: '85631',
+        paymentStatus : 'Due 5',
+        shipping: 'deliverd'
+    },
+    {
+        productName : 'Product name 6',
+        productNumber: '85631',
+        paymentStatus : 'Due 6',
+        shipping: 'Pending'
+    },
+]
